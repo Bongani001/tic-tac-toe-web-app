47a5a7be16cf3d809e3983cdd1d945aae6c6faaa
Bongani001 <bbongs421@gmail.com>
2023-01-03 20:47:21 +0200

    Modify files

---
+++ main.js
@@ -0,0 +1,25 @@
+// const gameboard = [];
+
+// const player = ((name, x) => {  // Create a player
+//     const getName = () => name;
+//     const getPiece = () => x;
+//     return getName, getPiece;
+// })();
+
+// const bob = player('boby', 'x');
+// console.log(bob);
+
+
+const gameBoard = (() => {
+
+    const playerFormation = (marker) => { //function for player creation
+        return marker;
+    };
+
+    const player1 = playerFormation('X'); // Create player 1
+    const player2 = playerFormation('O');; // Create player 2
+
+    let boardArr = [];
+
+
+})();
