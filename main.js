ddfb1a220683258079558c563d4374f820f4f32b
Bongani001 <bbongs421@gmail.com>
2023-01-04 11:00:45 +0200

    Edit files

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ const gameBoard = (() => {
 
     let boardArr = [];
 
+    const possibleCombinations = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [7,4,1],
+        [8,5,2],
+        [9,6,3],
+        [7,5,3],
+        [9,5,1],
+    ]
+
    return { 
    player1: player1,
    player2: player2
