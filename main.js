cf19d22a7f00fdd466d35300b337d76430d66cf2
Bongani001 <bbongs421@gmail.com>
2023-01-08 11:42:28 +0200

    Edit file

--- main.js
+++ main.js
@@ -7,43 +7,93 @@ const gameBoard = (() => {
     
     // Create player 1 and player 2
     const player1 = playerFormation('X'); 
-    const player2 = playerFormation('O');; 
+    const player2 = playerFormation('O');
 
     // Create combinations to match who is the winner
     const possibleCombinations = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [7,4,1],
-        [8,5,2],
-        [9,6,3],
-        [7,5,3],
-        [9,5,1],
-    ]
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8],
+    ];
 
     // An array to be used to match who is the winner
     let boardArr = [];
 
-    console.log(boardArr);
-//    return { 
-//    player1: player1,
-//    player2: player2
-//    };
-
-    let markerGrid = document.querySelectorAll('.marker-grid');
-    let i = 0;
-
-    markerGrid.forEach(marker => {
-
-        marker.dataset.markerNum = i;
-        boardArr.push(marker);
-        marker.addEventListener('click', () => {
-            marker.textContent = 'x';
-            boardArr[marker.dataset.markerNum] = marker.textContent;
-            console.log(boardArr);
-        });
-        i++;
-   });
-
-
+    return { 
+        boardArr,
+        player1,
+        player2,
+        possibleCombinations
+    };
 })();
+
+let markerGrid = document.querySelectorAll('.marker-grid');
+let i = 0;
+let currentPlayer = gameBoard.player1;
+let see = '';
+let winnerAnnouncement = document.querySelector('.winner-announcement');
+
+
+// Display markers on the display onclick
+markerGrid.forEach(marker => {
+    marker.dataset.markerNum = i;
+    gameBoard.boardArr.push(marker);
+    marker.addEventListener('click', (e) => {
+        //e.disabled = true;
+        //marker.textContent = currentPlayer;
+        // Change between players
+        if (currentPlayer === gameBoard.player1 && marker.textContent === '') {
+            marker.textContent = currentPlayer;
+            currentPlayer = gameBoard.player2;
+        } else if (currentPlayer === gameBoard.player2 && marker.textContent === '') {
+            marker.textContent = currentPlayer;
+            currentPlayer = gameBoard.player1;
+        } else {
+            return;
+        };
+        gameBoard.boardArr[marker.dataset.markerNum] = marker.textContent;
+        console.log(gameBoard.boardArr);
+        
+        
+        // Check for the winner
+        gameBoard.possibleCombinations.forEach(combinations => {
+            let first = combinations[0];
+            let second = combinations[1];
+            let third = combinations[2];
+            if (gameBoard.boardArr[first]=== gameBoard.player1) {
+                if (gameBoard.boardArr[second] === gameBoard.player1) {
+                    if (gameBoard.boardArr[third] === gameBoard.player1) {
+                        winnerAnnouncement.textContent = 'Player 1 is the winner';
+                        console.log('wowX');
+                    };
+                };
+            } else if (gameBoard.boardArr[first]=== gameBoard.player2) {
+                if (gameBoard.boardArr[second] === gameBoard.player2) {
+                    if (gameBoard.boardArr[third] === gameBoard.player2) {
+                        winnerAnnouncement.textContent = 'Player 2 is the winner';
+                        console.log('wowO');
+                    };
+                };
+            } else {
+                return;
+            };
+        });
+    });
+    i++;
+});
+
+// let winnerAnnouncement = document.querySelectorAll('.winner-announcement');
+
+// possibleCombinations.forEach(combinations => {
+//     combinations.forEach(combination => {
+//         if (gameBoard.player1 in gameBoard.boardArr[combination]) {
+//             winnerAnnouncement.textContent = 'Player 1 is the winner';
+//         };
+//     });
+// });
+
